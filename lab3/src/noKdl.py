358fe9bf61b98d1c9777a0e7406dcba5916794d3
klaudiii <klaudiastp@gmail.com>
2020-04-21 20:23:04 +0200

    kolejna wersja

--- lab3/src/noKdl.py
+++ lab3/src/noKdl.py
@@ -1,76 +1,89 @@
 #! /usr/bin/python
 
-import json
 import rospy
+import json
 import os
-from sensor_msgs.msg import JointState
 from tf.transformations import *
-from visualization_msgs.msg import Marker
+from sensor_msgs.msg import JointState
 from geometry_msgs.msg import PoseStamped
 
+xaxis, yaxis, zaxis = (1, 0, 0), (0, 1, 0), (0, 0, 1)
 
-def isPositionAvailable(data):
-    i=0
-    for instance in restrictionsJson:
-        oneInstance= json.loads(json.dumps(instance))
-        if data.position[i]>oneInstance["forward"] or data.position[i]<oneInstance["backward"] :
-            return False
-        i=i+1
+
+def correct(pose):
+    if pose.position[0] < rest['i1'][0] or pose.position[0] > rest['i1'][1]:
+        return False
+
+    if pose.position[1] < rest['i2'][0] or pose.position[1] > rest['i2'][1]:
+        return False
+
+    if pose.position[2] < rest['i3'][0] or pose.position[2] > rest['i3'][1]:
+        return False
+
     return True
-def callback(data):
-    if isPositionAvailable(data)==False:
-        rospy.logerr("Position is not available: " + str(data))
+
+
+def kinematics(data):
+    if not correct(data):
+        rospy.logerr('Incorrect position! ' + str(data))
         return
-    T = translation_matrix((0, 0, 0));
-    
-    i=0
-    for instance in dhJson:
-        oneInstance= json.loads(json.dumps(instance))
-        a = oneInstance["a"]
-        d = oneInstance["d"]
-        alpha=oneInstance["alpha"]
-        theta = oneInstance["theta"]
 
-        matrixD= translation_matrix((0, 0, d*(1+data.position[i])))
-        matrixTheta = rotation_matrix(theta, zaxis)
-        matrixA = translation_matrix((a, 0, 0))
-        matrixAlpha = rotation_matrix(alpha, xaxis)
+    a, d, al, th = params['i1']
+    al, a, d, th = float(al), float(a), float(d), float(th)
+    tz = translation_matrix((0, 0, d))
+    rz = rotation_matrix(data.position[0], zaxis)
+    tx = translation_matrix((a, 0, 0))
+    rx = rotation_matrix(al, xaxis)
+    T1 = concatenate_matrices(rx, tx, rz, tz)
 
-        TransMatrix = concatenate_matrices(matrixA,matrixAlpha,matrixTheta, matrixD)
-        T=concatenate_matrices(T, TransMatrix)
-        i=i+1
-        
-    
-    x, y, z = translation_from_matrix(T)
-    qx, qy, qz, qw = quaternion_from_matrix(T)
+    a, d, al, th = params['i2']
+    al, a, d, th = float(al), float(a), float(d), float(th)
+    tz = translation_matrix((0, 0, d))
+    rz = rotation_matrix(data.position[1], zaxis)
+    tx = translation_matrix((a, 0, 0))
+    rx = rotation_matrix(al, xaxis)
+    T2 = concatenate_matrices(rx, tx, rz, tz)
+
+    a, d, al, th = params['i3']
+    al, a, d, th = float(al), float(a), float(d), float(th)
+    tz = translation_matrix((0, 0, data.position[2]))
+    rz = rotation_matrix(th, zaxis)
+    tx = translation_matrix((a, 0, 0))
+    rx = rotation_matrix(al, xaxis)
+    T3 = concatenate_matrices(rx, tx, rz, tz)
+
+    Tk = concatenate_matrices(T1, T2, T3)
+    x, y, z = translation_from_matrix(Tk)
+    q_x, q_y, q_z, q_w = quaternion_from_matrix(Tk)
+
     pose = PoseStamped()
     pose.header.frame_id = 'base_link'
     pose.header.stamp = rospy.Time.now()
-    
     pose.pose.position.x = x
     pose.pose.position.y = y
     pose.pose.position.z = z
-    pose.pose.orientation.x = qx
-    pose.pose.orientation.y = qy
-    pose.pose.orientation.z = qz
-    pose.pose.orientation.w = qw
+    pose.pose.orientation.x = q_x
+    pose.pose.orientation.y = q_y
+    pose.pose.orientation.z = q_z
+    pose.pose.orientation.w = q_w
     pub.publish(pose)
-    
+
 
 
 if __name__ == '__main__':
-    xaxis, yaxis, zaxis = (1, 0, 0), (0, 1, 0), (0, 0, 1)
-    rospy.init_node('NONKDL_DKIN', anonymous=False)
-    dhJson ={}
-    restrictionsJson ={}
-    with open(os.path.dirname(os.path.realpath(__file__)) + '/../restrictions.json', 'r') as file:
-        restrictionsJson= json.loads(file.read())
-   
-    with open(os.path.dirname(os.path.realpath(__file__)) + '/../dh_data.json', 'r') as file:
-        dhJson= json.loads(file.read())
+    rospy.init_node('NONKDL_KIN', anonymous=False)
 
     pub = rospy.Publisher('NoKdlAxes', PoseStamped, queue_size=10)
-    rospy.Subscriber("joint_states", JointState , callback)
-    
-   
+
+    rospy.Subscriber('joint_states', JointState, kinematics)
+
+    params = {}
+    print os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.dirname(os.path.realpath(__file__)) + '/../dh.json', 'r') as file:
+        params = json.loads(file.read())
+
+    rest = {}
+    with open(os.path.dirname(os.path.realpath(__file__)) + '/../rest.json', 'r') as file:
+        rest = json.loads(file.read())
+
     rospy.spin()
--- lab3/src/withKdl.py
+++ lab3/src/withKdl.py
@@ -1,86 +1,99 @@
 #! /usr/bin/python
 
-import json
 import rospy
-import PyKDL
+import json
 import os
-from sensor_msgs.msg import JointState
+import PyKDL as kdl
+import math
 from tf.transformations import *
-from visualization_msgs.msg import Marker
+from sensor_msgs.msg import JointState
 from geometry_msgs.msg import PoseStamped
 
-def isPositionAvailable(data):
-    i=0
-    for instance in restrictionsJson:
-        oneInstance= json.loads(json.dumps(instance))
-        if data.position[i]>oneInstance["forward"] or data.position[i]<oneInstance["backward"] :
-            return False
-        i=i+1
+xaxis, yaxis, zaxis = (1, 0, 0), (0, 1, 0), (0, 0, 1)
+
+
+def correct(pose):
+    if pose.position[0] < rest['i1'][0] or pose.position[0] > rest['i1'][1]:
+        return False
+
+    if pose.position[1] < rest['i2'][0] or pose.position[1] > rest['i2'][1]:
+        return False
+
+    if pose.position[2] < rest['i3'][0] or pose.position[2] > rest['i3'][1]:
+        return False
+
     return True
-def callback(data):
-    if isPositionAvailable(data)==False:
-        rospy.logerr("Position is not available: " + str(data))
+
+
+def forward_kinematics(data):
+    if not correct(data):
+        rospy.logerr('Incorrect position! ' + str(data))
         return
-    kdlChain =PyKDL.Chain()   
-    Frame = PyKDL.Frame();
 
-    d=0
-    theta=0
-    i=1
-    for instance in dhJson:
-        oneInstance= json.loads(json.dumps(instance))
-        lastD= d
-        lastTheta=theta
-        a = oneInstance["a"]
-        d = oneInstance["d"]
-        alpha=oneInstance["alpha"]
-        theta = oneInstance["theta"]
-        if i!= 1:
-            kdlChain.addSegment(PyKDL.Segment(PyKDL.Joint(PyKDL.Joint.TransZ), Frame.DH(a, alpha, lastD, lastTheta)))
-        i=i+1
-    kdlChain.addSegment(PyKDL.Segment(PyKDL.Joint(PyKDL.Joint.TransZ), Frame.DH(0, 0, d, theta)))
-      
-      
-    jointDisplacement = PyKDL.JntArray(kdlChain.getNrOfJoints())
+    kdlChain = kdl.Chain()
+    frameFactory = kdl.Frame();
 
-    jointDisplacement[0] = data.position[0]
-    jointDisplacement[1] = data.position[1]
-    jointDisplacement[2] = data.position[2]
+    frame0 = frameFactory.DH(0, 0, 0, 0);
+    joint0 = kdl.Joint(kdl.Joint.None)
+    kdlChain.addSegment(kdl.Segment(joint0, frame0))
 
-    fk = PyKDL.ChainFkSolverPos_recursive(kdlChain)
+    a, d, al, th = params['i2']
+    al, a, d, th = float(al), float(a), float(d), float(th)
+    frame1 = frameFactory.DH(a, al, d, th)
+    joint1 = kdl.Joint(kdl.Joint.RotZ)
+    kdlChain.addSegment(kdl.Segment(joint1, frame1))
 
-    finalFrame = PyKDL.Frame()
-    fk.JntToCart(jointDisplacement, finalFrame)
-    quaternion = finalFrame.M.GetQuaternion()
-    
+    a, d, al, th = params['i1']
+    al, a, d, th = float(al), float(a), float(d), float(th)
+    frame2 = frameFactory.DH(a, al, d, th)
+    joint2 = kdl.Joint(kdl.Joint.RotZ)
+    kdlChain.addSegment(kdl.Segment(joint2, frame2))
+
+    a, d, al, th = params['i3']
+    al, a, d, th = float(al), float(a), float(d), float(th)
+    frame3 = frameFactory.DH(a, al, d, th)
+    joint3 = kdl.Joint(kdl.Joint.TransZ)
+    kdlChain.addSegment(kdl.Segment(joint3, frame3))
+
+    jntAngles = kdl.JntArray(kdlChain.getNrOfJoints())
+    jntAngles[0] = data.position[0]
+    jntAngles[1] = data.position[1]
+    jntAngles[2] = -data.position[2] - d
+
+    fksolver = kdl.ChainFkSolverPos_recursive(kdlChain)
+    eeFrame = kdl.Frame()
+    fksolver.JntToCart(jntAngles, eeFrame)
+    quaternion = eeFrame.M.GetQuaternion()
+
     pose = PoseStamped()
     pose.header.frame_id = 'base_link'
     pose.header.stamp = rospy.Time.now()
-
-    pose.pose.position.x = finalFrame.p[0]
-    pose.pose.position.y = finalFrame.p[1]
-    pose.pose.position.z = finalFrame.p[2]
+    pose.pose.position.x = eeFrame.p[0]
+    pose.pose.position.y = eeFrame.p[1]
+    pose.pose.position.z = eeFrame.p[2]
     pose.pose.orientation.x = quaternion[0]
     pose.pose.orientation.y = quaternion[1]
     pose.pose.orientation.z = quaternion[2]
     pose.pose.orientation.w = quaternion[3]
     pub.publish(pose)
-    
+
 
 
 if __name__ == '__main__':
-    
-    rospy.init_node('KDL_DKIN', anonymous=False)
-    dhJson ={}
-    restrictionsJson ={}
-    with open(os.path.dirname(os.path.realpath(__file__)) + '/../restrictions.json', 'r') as file:
-        restrictionsJson= json.loads(file.read())
-   
-    with open(os.path.dirname(os.path.realpath(__file__)) + '/../dh_data.json', 'r') as file:
-        dhJson= json.loads(file.read())
+    rospy.init_node('KDL_KIN', anonymous=True)
 
     pub = rospy.Publisher('KdlAxes', PoseStamped, queue_size=10)
-    rospy.Subscriber("joint_states", JointState , callback)
-    
-   
+ 
+
+    rospy.Subscriber('joint_states', JointState, forward_kinematics)
+
+    params = {}
+    print os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.dirname(os.path.realpath(__file__)) + '/../dh.json', 'r') as file:
+        params = json.loads(file.read())
+
+    rest = {}
+    with open(os.path.dirname(os.path.realpath(__file__)) + '/../rest.json', 'r') as file:
+        rest = json.loads(file.read())
+
     rospy.spin()
